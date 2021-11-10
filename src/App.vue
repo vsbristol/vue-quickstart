6d33539dc9fdae51ecfd46fd5fa5fbfdf963c06f
Dillon Smart <dillon.smart@ouritservices.co.uk>
2021-11-10 13:05:49 +0000

    Routing and basic views added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
-  <div>
-      <div class="pt-12 pb-6">
-          <svg class="w-52 mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69"><path d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z" fill="#41b883"/><path d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z" fill="#34495e"/></svg>
-      </div>
-      <div class="text-center">
-          <h1 class="text-4xl">Vue.js Quickstart</h1>
-      </div>
-  </div>
+    <router-view />
 </template>
 
 <script>
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="pt-12 pb-6 text-center">
+            <h1 class="text-4xl mb-2">Login</h1>
+            <p class="text-blue-500"><router-link to="/">Back to Home</router-link></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
