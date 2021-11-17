efb19466e04f6257244c74ae36afed9b33cadd08
Dillon Smart <dillon.smart@ouritservices.co.uk>
2021-11-17 19:40:16 +0000

    Sidebar component html and close method

--- src/components/layout/SidebarComponent.vue
+++ src/components/layout/SidebarComponent.vue
@@ -1,10 +1,22 @@
 <template>
+    <div>
+        <div class="bg-gray-900 opacity-20 h-full w-full fixed top-0 left-0 z-10" v-on:click="close"></div>
+        <div class="bg-white w-72 h-full fixed top-0 left-0 shadow-lg z-20" id="nav">
+            <ul class="text-gray-700 my-6">
 
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "SidebarComponent"
+    name: "SidebarComponent",
+    methods: {
+        close() {
+            this.$parent.menuState = false;
+        }
+    }
 }
 </script>
 
