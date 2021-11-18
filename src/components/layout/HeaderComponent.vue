993993893f6ae4b1753f24f8fc109746533c117e
Dillon Smart <dillon.smart@ouritservices.co.uk>
2021-11-18 08:44:36 +0000

    Header and sidebar changes

--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -13,7 +13,12 @@
                     <h1 class="capitalize">{{ (title) ? title : $route.name }}</h1>
                 </div>
                 <div class="flex-1 text-right justify-end">
-                    <div>
+                    <div v-if="!$store.getters.user">
+                        <button class="ml-4 rounded">
+                            <router-link class="block px-2 pb-1 h-8 top-1 relative" to="/login">Login</router-link>
+                        </button>
+                    </div>
+                    <div v-if="$store.getters.user">
                         <button class="ml-4 px-2 h-8 pb-1 rounded hover:bg-primary-hover" v-on:click="(showUserMenu) ? showUserMenu = false : showUserMenu = true;">
                             <span class="inline relative" style="top: 2px">{{ $store.getters.user.name }}</span>
                             <svg xmlns="http://www.w3.org/2000/svg" class="inline ml-2 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -29,7 +34,10 @@
                 </div>
             </div>
         </header>
-        <sidebar-component v-if="menuState" @close="closeSidebar"></sidebar-component>
+
+        <div class="bg-gray-900 opacity-20 h-full w-full fixed top-0 left-0 z-10" v-on:click="closeSidebar" v-if="menuState"></div>
+        <sidebar-component v-if="menuState"></sidebar-component>
+
     </div>
 </template>
 
@@ -39,6 +47,7 @@ import SidebarComponent from "./SidebarComponent";
 export default {
     name: "HeaderComponent",
     components: {SidebarComponent},
+    props: ['title'],
     data: function() {
         return {
             menuState: false,
@@ -59,5 +68,37 @@ export default {
 </script>
 
 <style scoped>
+.slide-enter-active,
+.slide-leave-active
+{
+    transition: transform 0.2s ease;
+}
 
+.slide-enter,
+.slide-leave-to {
+    transform: translateX(-100%);
+    transition: all 150ms ease-in 0s
+}
+
+.sidebar-backdrop {
+    background-color: rgba(0,0,0,.5);
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    cursor: pointer;
+}
+
+.sidebar-panel {
+    overflow-y: auto;
+    background-color: #130f40;
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100vh;
+    z-index: 999;
+    padding: 3rem 20px 2rem 20px;
+    width: 300px;
+}
 </style>
--- src/components/layout/SidebarComponent.vue
+++ src/components/layout/SidebarComponent.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-        <div class="bg-gray-900 opacity-20 h-full w-full fixed top-0 left-0 z-10" v-on:click="close"></div>
         <div class="bg-white w-72 h-full fixed top-0 left-0 shadow-lg z-20" id="nav">
-            <ul class="text-gray-700 my-6">
+            <div class="my-12 text-center">
+                <svg class="w-24 mx-auto mb-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69"><path d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z" fill="#41b883"/><path d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z" fill="#34495e"/></svg>
+                <h1 class="font-bold text-xl">Vue.js Quickstart</h1>
+            </div>
+            <ul class="text-gray-700">
 
             </ul>
         </div>
@@ -21,5 +24,22 @@ export default {
 </script>
 
 <style scoped>
+.slide-fade-enter-active {
+    transition: all .3s ease-in-out;
+}
+.slide-fade-leave-active {
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+    /* .slide-fade-leave-active below version 2.1.8 */ {
+    transform: translateX(10px);
+    opacity: 0;
+}
 
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+}
 </style>
